c0fa84f1cebe2dbea8296b7ca36149d0fbc556e7
zwc <zwczou@gmail.com>
2016-08-31 18:25:48 +0800

    增加README

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+
+setup(
+        name='libweixin',
+        py_modules=['weixin'],
+        version='0.1.0',
+        description='Weixin for python',
+        author='zwczou',
+        author_email='zwczou@gmail.com',
+        url='https://github.com/zwczou/weixin-python',
+        keywords=['weixin', 'weixin pay', 'weixin login'],
+        classifiers=[],
+)
