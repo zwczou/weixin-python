7ceaec199ecdeb431bc77ba67b30d1ae53e95167
zwc <zwczou@gmail.com>
2016-09-01 14:19:39 +0800

    增加描述

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@
 from setuptools import setup, find_packages
 
 
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath) as f:
+        return f.read()
+
 setup(
     name='weixin-python',
-    description='Weixin for python',
+    description='Weixin for Python',
+    long_description=fread('README.rst'),
     license = 'BSD',
     packages = find_packages(),
-    version='0.2.2',
+    version='0.2.4',
     author='zwczou',
     author_email='zwczou@gmail.com',
     url='https://github.com/zwczou/weixin-python',
@@ -17,7 +23,6 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'Flask',
     ],
     classifiers=[],
 )
